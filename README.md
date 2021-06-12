cc8976f4b6c5161eada44e3d2036e2f75920c952
agb1986 <abrolly1986@gmail.com>
2021-06-12 14:59:10 +0100

    Google OAuth section

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fullstack with NodeJS/React
+
+[Udemy](https://www.udemy.com/course/node-with-react-fullstack-web-development/)
+[Heroku Site](https://agb-dev-86-fs-node-react.herokuapp.com/)
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 
-        hi: 'there,',
-        how: 'are you?'
-    });
-});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleCilentID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(`ACCESS TOKEN: ${accessToken}`);
+            console.log(`REFRESH TOKEN: ${refreshToken}`);
+            console.log('PROFILE: ', profile);
+        }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email'],
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
