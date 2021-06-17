a235cf7eeb527aecf03bae651489eea575ab299b
agb1986 <abrolly1986@gmail.com>
2021-06-17 17:05:23 +0100

    Adding mongoDB and User model

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./src/server/models/users.model');
 require('./src/server/services/passport.service');
+
+const keys = require('./src/config/keys')
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 const app = express();
 
 require('./src/server/routes/auth.route')(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
