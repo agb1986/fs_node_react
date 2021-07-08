6e2d9a1e13c065032a11453eceb650870d56ff7c
agb1986 <abrolly1986@gmail.com>
2021-07-08 16:09:02 +0100

    Update to keys file with PROD/DEV logic

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,9 +1,17 @@
-const keysDev = require('./keys.dev');
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        googleCilentID: process.env.GOOGLE_CILENT_ID,
+        googleClientSecret: process.env.GOOGLE_CILENT_SECRET,
+        mongoURI: process.env.MONGO_URI,
+        cookieKey: process.env.COOKIE_KEY,
+    };
+} else {
+    const keysDev = require('./keys.dev');
 
-module.exports = {
-    googleCilentID: process.env.GOOGLE_CILENT_ID || keysDev.googleCilentID,
-    googleClientSecret:
-        process.env.GOOGLE_CILENT_SECRET || keysDev.googleClientSecret,
-    mongoURI: process.env.MONGO_URI || keysDev.mongoURI,
-    cookieKey: process.env.COOKIE_KEY || keysDev.cookieKey,
-};
+    module.exports = {
+        googleCilentID: keysDev.googleCilentID,
+        googleClientSecret: keysDev.googleClientSecret,
+        mongoURI: keysDev.mongoURI,
+        cookieKey: keysDev.cookieKey,
+    };
+}
